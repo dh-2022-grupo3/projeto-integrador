0780adfe5cf54ca8c12341cdad639430dc280694
Gregorio Ramos <gregorio236@gmail.com>
2022-03-12 13:48:09 -0300

    Implementado head em comum com todas as views;

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => { res.render('login'); });
-router.get('/cadastro', (req, res) => { res.render('cadastro'); });
-router.get('/chart', (req, res) => { res.render('chart'); });
-router.get('/customizacao', (req, res) => { res.render('customizacao'); });
-router.get('/forgot', (req, res) => { res.render('forgot'); });
-router.get('/home', (req, res) => { res.render('home'); });
-router.get('/login', (req, res) => { res.render('login'); });
+router.get('/', (req, res) => { res.render('login', { styles: ['login'] }); });
+router.get('/cadastro', (req, res) => { res.render('cadastro', { styles: ['cadastro'] }); });
+router.get('/chart', (req, res) => { res.render('chart', { styles: ['chart'] }); });
+router.get('/customizacao', (req, res) => { res.render('customizacao', { styles: ['customizacao'] }); });
+router.get('/forgot', (req, res) => { res.render('forgot', { styles: ['forgot'] }); });
+router.get('/home', (req, res) => { res.render('home', { styles: ['home'] }); });
+router.get('/login', (req, res) => { res.render('login', { styles: ['login'] }); });
 
 module.exports = router;
