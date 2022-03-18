6d3ecd64dad3cfa1b06a0439b81b9ff329a2a4ee
kicomartins <kicomartins@gmail.com>
2022-03-17 21:46:00 -0300

    criacao da rota login

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./src/routes/index');
+const loginRouter = require('./src/routes/login');
 const homeRouter = require('./src/routes/home');
 const customizacaoRouter = require('./src/routes/customizacao');
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
 app.use('/home', homeRouter);
 app.use('/customizacao', customizacaoRouter);
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,5 @@ router.get('/', (req, res) => { res.render('login', { styles: ['login'] }); });
 router.get('/cadastro', (req, res) => { res.render('cadastro', { styles: ['cadastro'] }); });
 router.get('/chart', (req, res) => { res.render('chart', { styles: ['chart'] }); });
 router.get('/forgot', (req, res) => { res.render('forgot', { styles: ['forgot'] }); });
-router.get('/login', (req, res) => { res.render('login', { styles: ['login'] }); });
 
 module.exports = router;
