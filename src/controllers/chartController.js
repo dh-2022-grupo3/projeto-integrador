51a66ece1b04dd5f7f6554541911075bcde25732
David Barbato <daviddev1306@gmail.com>
2022-03-19 21:10:51 -0300

    passar valores atraves do controller

--- src/controllers/chartController.js
+++ src/controllers/chartController.js
@@ -1,7 +1,14 @@
 const chartController = {
   index: (req, res) => {
+    const valores = [
+      {
+        valor: 40,
+        descricao: 'Previsao de Gastos',
+      },
+    ];
     res.render('chart', {
       styles: ['chart'],
+      valores,
     });
   },
 };
