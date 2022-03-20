f9e807deff84e0965aa0210154007c4c3110550e
David Barbato <daviddev1306@gmail.com>
2022-03-20 19:01:52 -0300

    Melhoramento da estrutura e logica

--- src/controllers/chartController.js
+++ src/controllers/chartController.js
@@ -1,32 +1,27 @@
 const chartController = {
   index: (req, res) => {
-    const valoresPrevisao =
-    {
-      valor: '500,00',
-      descricao: 'Previsão de Gastos',
-      icone: 'msentiment_satisfied_alt',
-    };
+    const dados = [
+      {
+        valor: '500,00',
+        descricao: 'Previsão de Gastos',
+        icone: 'msentiment_satisfied_alt',
+      },
+      {
+        valor: '125,80',
+        descricao: 'Pagamentos',
+        icone: 'payments',
+      },
+      {
+        valor: '374,20',
+        descricao: 'Balanço',
+        icone: 'account_balance_wallet',
 
-    const valoresPagamentos =
-    {
-      valor: '125,80',
-      descricao: 'Pagamentos',
-      icone: 'payments',
-    };
-
-    const valoresBalanco =
-    {
-      valor: '374,20',
-      descricao: 'Balanço',
-      icone: 'account_balance_wallet',
-
-    };
+      },
+    ];
 
     res.render('chart', {
       styles: ['chart'],
-      valoresPrevisao,
-      valoresPagamentos,
-      valoresBalanco,
+      dados,
     });
   },
 };
