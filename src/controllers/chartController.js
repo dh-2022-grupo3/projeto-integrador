be774ee225e464dca823e750f4a1a860d587760f
David Barbato <daviddev1306@gmail.com>
2022-03-20 18:29:43 -0300

    melhoranto da logica

--- src/controllers/chartController.js
+++ src/controllers/chartController.js
@@ -1,27 +1,23 @@
 const chartController = {
   index: (req, res) => {
-    const valoresPrevisao = [
-      {
-        valor: '500,00',
-        descricao: 'Previsão de Gastos',
-      },
-    ];
+    const valoresPrevisao =
+    {
+      valor: '500,00',
+      descricao: 'Previsão de Gastos',
+    };
 
-    const valoresPagamentos = [
-      {
-        valor: '125,80',
-        descricao: 'Pagamentos',
-      },
+    const valoresPagamentos =
+    {
+      valor: '125,80',
+      descricao: 'Pagamentos',
+    };
 
-    ];
+    const valoresBalanco =
+    {
+      valor: '374,20',
+      descricao: 'Balanço',
 
-    const valoresBalanco = [
-      {
-        valor: '374,20',
-        descricao: 'Balanço',
-
-      },
-    ];
+    };
 
     res.render('chart', {
       styles: ['chart'],
