556bcb0d73d7e5d696283fec5061282a120ac495
David Barbato <daviddev1306@gmail.com>
2022-04-09 15:42:02 -0300

    add logica para cadastrar usuario

--- src/controllers/cadastroController.js
+++ src/controllers/cadastroController.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const bcrypt = require('bcryptjs');
+
+const usuarioJson = path.join('usuarios.json');
 const cadastroController = {
   index: (req, res) => {
     res.render('cadastro', {
       styles: ['cadastro'],
     });
+  },
+  salvarForm: (req, res) => {
+    const { nome, email, senha } = req.body;
+    const senhaCryp = bcrypt.hashSync(senha, 8);
+    const usuario = JSON.stringify({ nome, email, senha: senhaCryp });
+
+    fs.writeFileSync(usuarioJson, usuario);
+    res.send('Cadastrado com sucesso');
   },
 };
 
