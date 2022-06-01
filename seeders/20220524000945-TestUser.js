45b3b587e6a20760d72463991f0d1f2a7b7c4226
Gregorio Ramos <gregorio236@gmail.com>
2022-06-01 20:06:37 -0300

    Adicionado seed usuario teste

---
+++ seeders/20220524000945-TestUser.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("usuario", [
+      {
+        nome: "Teste",
+        sobrenome: "Testando",
+        email: "teste@teste.com",
+        senha: "$2a$08$gegY0ClMATve5pjCRq7CSOBDBdTpkmhrDbP0BhIAEHgmZ9XAeZCPK", // 123456
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("usuario", { nome: "teste@teste.com" });
+  },
+};
