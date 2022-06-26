279445b470610c13dcd1f7d5a107de8bb2e42353
Gregorio Ramos <gregorio236@gmail.com>
2022-06-26 20:22:24 -0300

    Implement customization;

--- src/controllers/customizacaoController.js
+++ src/controllers/customizacaoController.js
@@ -4,23 +4,22 @@ const customizacaoController = {
   index: (req, res) => {
     res.render("customizacao", {
       styles: ["customizacao"],
-      user,
+      usuario: req.session.usuario,
       title: "Customização",
       name: req.session.usuario.nome,
     });
   },
 
   salvarCusotmizacao: async (req, res) => {
-    const { nome, sobrenome, escolaridade, fonte, renda, descricao } = req.body;
+    const { nome, sobrenome } = req.body;
 
-    await Customizacao.create({
-      nome,
-      sobrenome,
-      escolaridade,
-      fonte,
-      renda,
-      descricao,
-    });
+    await Usuario.update(
+      { nome, sobrenome },
+      { where: { id: req.session.usuario.id } }
+    );
+
+    req.session.usuario.nome = nome;
+    req.session.usuario.sobrenome = sobrenome;
 
     return res.redirect("/customizacao");
   },
